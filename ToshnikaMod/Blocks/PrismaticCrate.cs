137bd5fcee32ec5fd2bf9fb905c36288b82bf36c
toshnika <36898305+toshnika@users.noreply.github.com>
2019-12-22 22:18:24 -0700

    Add files via upload

---
+++ ToshnikaMod/Blocks/PrismaticCrate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace ToshnikaMod.Blocks
+{
+    internal sealed class PrismaticCrate : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileLighted[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileFrameImportant[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+            TileObjectData.newTile.Origin = new Point16(0, 1);
+            TileObjectData.newTile.CoordinateHeights = new[] { 16, 18 };
+            TileObjectData.newTile.HookCheck = new PlacementHook(new Func<int, int, int, int, int, int>(Chest.FindEmptyChest), -1, 0, true);
+            TileObjectData.newTile.HookPostPlaceMyPlayer = new PlacementHook(new Func<int, int, int, int, int, int>(Chest.AfterPlacement_Hook), -1, 0, false);
+            TileObjectData.newTile.AnchorInvalidTiles = new[] { 127 };
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.LavaDeath = false;
+            TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.SolidWithTop | AnchorType.SolidSide, TileObjectData.newTile.Width, 0);
+            TileObjectData.addTile(Type);
+            ModTranslation name = CreateMapEntryName("Crate");
+            name.SetDefault("Crate");
+            AddMapEntry(new Color(50, 50, 50), name);
+
+                            }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(i * 16, j * 16, 20, 28, mod.ItemType("PrismaticCrate"));
+        }
+    }
+}
---
+++ ToshnikaMod/Blocks/TruePStone.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using static Terraria.ModLoader.ModContent;
+
+namespace ToshnikaMod.Blocks
+{
+    public class TruePStone : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileShine[Type] = 1100;
+            Main.tileSolid[Type] = false;
+            Main.tileSolidTop[Type] = false;
+            Main.tileFrameImportant[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style1x1);
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.LavaDeath = false;
+            TileObjectData.addTile(Type);
+            AddMapEntry(new Color(255, 0, 0));
+        }
+
+        public override bool Drop(int i, int j)
+        {
+            Tile t = Main.tile[i, j];
+            int style = t.frameX / 18;
+            if (style == 0) 
+            {
+                Item.NewItem(i * 16, j * 16, 16, 16, mod.ItemType("TruePhilosophersStone"));
+            }
+            return base.Drop(i, j);
+        }
+        public override void NearbyEffects(int i, int j, bool closer)
+        {
+            if (closer)
+            {
+                Player player = Main.player[Main.myPlayer];
+                int style = Main.tile[i, j].frameX / 15;
+                player.AddBuff(mod.BuffType("TruePStoneBuff"), 90, true);
+            }
+        }
+    }
+}
